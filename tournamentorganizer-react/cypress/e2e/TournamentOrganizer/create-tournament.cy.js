15b05c88f204f415a3919f9f9bc514329c6ffa7d
TimIraca <80639859+TimIraca@users.noreply.github.com>
2024-12-13 00:27:10 +0100

    test if the created tournament is present as a card

--- tournamentorganizer-react/cypress/e2e/TournamentOrganizer/create-tournament.cy.js
+++ tournamentorganizer-react/cypress/e2e/TournamentOrganizer/create-tournament.cy.js
@@ -1,11 +1,20 @@
 describe('Create Tournament', () => {
-    it('should navigate to the tournaments page', () => {
+    it('should create a tournament successfully', () => {
+        cy.intercept('POST', '**/api/Tournaments').as('createTournament')
+
         cy.visit('localhost:3000/tournaments')
-        cy.get('.bg-primary').click()
+        cy.get('[data-cy="create-tournament-button"]').click()
         cy.get('div[role="dialog"]').should('be.visible')
         cy.get('input[id="name"]').type('Tournament 1')
-        cy.get('input[id="startDate"]').type('2024-12-11')
+        cy.get('input[id="startDate"]').type('2024-12-12')
         cy.get('button[type="submit"]').click()
+        
+        cy.wait('@createTournament').then((interception) => {
+            expect(interception.response.statusCode).to.eq(201)
+            const tournamentId = interception.response.body.id
+        
+            cy.get(`[href="/tournaments/${tournamentId}"]`).should('exist')
+        })
         cy.get('div[role="dialog"]').should('not.exist')
     })
 })
