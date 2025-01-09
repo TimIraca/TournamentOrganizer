e79a396ab538218a334953a5a635e8ecc7087126
TimIraca <80639859+TimIraca@users.noreply.github.com>
2025-01-09 23:13:36 +0100

    add auth to integration tests

--- TournamentOrganizer/TournamentOrganizer.api.IntegrationTests/TournamentOrganizerWebApplicationFactory.cs
+++ TournamentOrganizer/TournamentOrganizer.api.IntegrationTests/TournamentOrganizerWebApplicationFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
@@ -26,6 +27,13 @@ namespace TournamentOrganizer.api.IntegrationTests
                         "Server=localhost,1433;Database=TournamentDb_Test;User=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True"
                     );
                 });
+                services
+                    .AddAuthentication("TestScheme")
+                    .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                        "TestScheme",
+                        options => { }
+                    );
+
                 TournamentContext dbContext = CreateDbContext(services);
                 dbContext.Database.EnsureDeleted();
                 dbContext.Database.Migrate();
